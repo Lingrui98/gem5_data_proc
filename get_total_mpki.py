e710b8a419b36ca214d8b8347e0d147f473d4761
Lingrui98 <goulingrui15@gmail.com>
2019-05-06 21:00:10 +0800

    Arg -s, output format

--- get_total_mpki.py
+++ get_total_mpki.py
@@ -3,6 +3,7 @@ import os
 import csv
 import numpy as np
 import pandas as pd
+import argparse
 
 
 def get_files():
@@ -12,13 +13,28 @@ def get_files():
             files.append(f)
     return files
 
-files = get_files()
-print(files)
-
-for f in files:
-    print(f)
+def get_mpki(f):
     df = pd.read_csv('./data/'+f, index_col=0)
     df['per_mpki'] = df['iew.branchMispredicts'] / 200000
-    df['per_mpki'].round(2)
     df.to_csv(f)
-    print('\ttotal_mean_mpki: ', df['per_mpki'].mean().round(2),'\n')
+    print('%s: %.2f' % (f[:-4], df['per_mpki'].mean().round(2)))
+
+def main():
+    print('')
+    parser = argparse.ArgumentParser(usage='-s')
+    parser.add_argument('-s', '--specified-test', action='store',
+                        help='specify a test')
+
+    opt = parser.parse_args()
+    if opt.specified_test != None:
+        files = [opt.specified_test+'.csv']
+    else:
+        files = get_files()
+
+    files.sort()
+
+    for f in files:
+        get_mpki(f)
+
+if __name__ == '__main__':
+    main()
